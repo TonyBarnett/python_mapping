bf4c6bf0e8199affe8da7b0fc470eee772780c5c
Tony Barnett <tony.barnett@ukplc.net>
2015-05-26 20:47:46 +0100

    adding my first classifier (exciting), let's start simple with KNN

---
+++ kNearestNeighbour.py
@@ -0,0 +1,17 @@
+from Classifier import Classifier
+from Document import Document
+
+
+class KNearestNeighbour(Classifier):
+
+    def __init__(self):
+        super(KNearestNeighbour, self).__init__("KNearestNeighbour")
+
+    def train(self, training_documents: list):
+        pass
+
+    def run_items(self, validation_documents: list):
+        pass
+
+    def get_result_for_item(self, item_id=None):
+        pass
