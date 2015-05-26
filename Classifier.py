4921327aef7a5d124ea3acddff980c01b857fdaa
Tony Barnett <tony.barnett@ukplc.net>
2015-05-26 20:47:26 +0100

    little bits

--- Classifier.py
+++ Classifier.py
@@ -1,13 +1,14 @@
 class Classifier:
     """ The base class for a classifier, each method must be overridden (hence all the notImplementedErrors)
     """
-    def __init__(self):
-        self.name = None
+    def __init__(self, name):
+        self.name = name
+        self.classes = list()
 
-    def train(self, training_documents):
+    def train(self, training_documents: list):
         raise NotImplementedError()
 
-    def run_items(self, validation_documents):
+    def run_items(self, validation_documents: list):
         raise NotImplementedError()
 
     def get_result_for_item(self, item_id=None):
